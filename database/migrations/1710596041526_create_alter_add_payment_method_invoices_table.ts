a34d2667560f44cf43251eb1d5b8aab606ccb60d
jbarretodev <jbarretodev6105@gmail.com>
2024-04-24 15:05:34 -0400

    feature: progress

--- database/migrations/1710596041526_create_alter_add_payment_method_invoices_table.ts
+++ database/migrations/1710596041526_create_alter_add_payment_method_invoices_table.ts
@@ -5,7 +5,10 @@ export default class extends BaseSchema {
 
   async up() {
     this.schema.alterTable('invoices', (table) => {
-      table.enum('payment_method', ['biopago', 'Bs Efectvo', '$ efectivo', 'TDD', 'TDC'])
+      table
+        .enum('payment_method', ['biopago', 'Bs Efectvo', '$ efectivo', 'TDD', 'TDC', 'NINGUNO'])
+        .nullable()
+        .defaultTo('NINGUNO')
       table.string('num_operation').nullable()
     })
   }
